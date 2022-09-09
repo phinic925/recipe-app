a04b961776a44497d05d271455c3d234c6b16b28
phinic925 <phinicnyangate480@gmail.com>
2022-09-09 17:26:08 +0100

    styles search bar

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,4 +4,23 @@ function Home(){
 
     const[search,setSearch] = useState("");
     const[data] = useFetch("http://localhost:9292/recipes")
-    
+    return (
+        <>
+        <h1 className="heading"> Make Food! </h1>
+            <p className="para"> You know you can easily make food </p>
+            <div className="input-group col-xs-4 search">
+            <input
+            type="text"
+            placeholder="Search Product"
+            className="form-control"
+            value={search}
+            onChange={(e)=>setSearch(e.target.value) }
+        
+                
+                
+            />
+            <button className ="btn btn-primary" > search </button>
+
+     
+      
+            </div>
