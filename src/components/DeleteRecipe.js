98609722b232cbec6b7b0f44a54803eeeff27e41
phinic925 <phinicnyangate480@gmail.com>
2022-09-13 11:40:19 +0100

    add delete functionality

--- src/components/DeleteRecipe.js
+++ src/components/DeleteRecipe.js
@@ -2,10 +2,12 @@
 
 import React from 'react'
 
-const DeleteRecipe = ({recipe, onDeleteRecipe }) => {
+const DeleteRecipe = (onDeleteRecipe ) => {
+    const[toggle,setToggle]= useState(true)
+    const[search,setSearch] = useState("");
    
     function handleDeleteRecipe() {
-    fetch(`https://books-app-backend.herokuapp.com/book/${recipe.id}`, {
+    fetch(`http://localhost:9292/recipes/${recipe.id}`, {
     method:"DELETE",
     })
       .then(r => r.json())
@@ -13,20 +15,96 @@ const DeleteRecipe = ({recipe, onDeleteRecipe }) => {
       onDeleteRecipe(recipe)
       })
     }
+   
+    
+    function handleToggle() {
+        setToggle(!toggle)
+        
+    }
     
   return (
-    <div>
+    <>
+        <h1 className="heading"> Make Food! </h1>
+            <p className="para"> You know you can easily make food </p>
+            <div className="input-group col-xs-4 search">
+            <input
+            type="text"
+            placeholder="Search recipe"
+            className="form-control"
+            value={search}
+            onChange={(e)=>setSearch(e.target.value) }
+        
+                
+                
+            />
+            <button className ="btn btn-primary" > search </button>
+
+     
       
-        <div className="flex content-center justify-center mt-5">
-        <div className="span">
-        <p>Title: {book.title}</p>
-        <p>Publisher Name: {book.publisher}</p>
-        <p>Price: Ksh{ book.price}</p>
-        <button className='delete' onClick={handleDeleteRecipe} >Delete</button>
+            </div>
+            {data 
+
+         
+         
+&& <div className='row '>
+    
+    {data
+    
+    .filter((item) =>{
+        if (search === ""){
+            return item;
+        }
+        else if(item.name.toLowerCase().includes(search.toLowerCase())){
+            return item;
+        }
+    })
+    
+    .map(item=>(
+        
+       
+        
+        <div className="col-md-4 mr-3 " key={item.id}>
+         <div className="item"> 
+         <div className="image">  
+        <img src={item.image_url}/> 
+        <span className="close-btn">
+            <i className="fas fa-window-close"/>
+        </span>
+        </div>
+        <div>
+        <h3 className="header1"> {item.name} </h3>
+        
+        <h4 className="header2">{item.category}</h4>
+        </div>
+        <div>
+        <h5 className="header3"> instruction 
+        <span key={item.id}  onClick={() =>{handleToggle(item.id)}}>
+            <i className="fas fa-caret-square-down" />
+        </span>
+            </h5>
+            {toggle &&  (<p className="para1"> {item.instructions}</p>)}
+            <button className="btn btn-primary button" onClick={() => setReview(!review)} > Review</button>
+            {/* {review &&  (<Review/>)} */}
+            
       </div>
+      
+     
       </div>
-    </div>
-  )
+       
+        
+     
+
+ </div>
+
+))}
+
+
+
+</div>
+}
+        </>
+    )
+  
 }
 
 export default DeleteRecipe;
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,92 +1,27 @@
 import React ,{useState} from 'react';
+import DeleteRecipe from './DeleteRecipe';
 import useFetch from './useFetch';
-import Review from './Review';
+// import Review from './Review';
 function Home(){
 
-    const[search,setSearch] = useState("");
+    
     const[data] = useFetch("http://localhost:9292/recipes")
-    const[toggle,setToggle]= useState(true)
-    const[review,setReview] = useState(true)
+    
+    function handleDelete(deletedRecipe) {
+        const newRecipes = data.filter((recipe) => recipe.id !== deletedRecipe.id)
+        setData(newRecipes)
+      }
       
     return (
         <>
-        <h1 className="heading"> Make Food! </h1>
-            <p className="para"> You know you can easily make food </p>
-            <div className="input-group col-xs-4 search">
-            <input
-            type="text"
-            placeholder="Search recipe"
-            className="form-control"
-            value={search}
-            onChange={(e)=>setSearch(e.target.value) }
+        <div className="row">
+            {data.map((recipe) =>(
+                <DeleteRecipe id={recipe.id} key={recipe.id} recipe={recipe} onDeleteRecipe={handleDelete}/>
+            ))}
         
-                
-                
-            />
-            <button className ="btn btn-primary" > search </button>
-
-     
-      
-            </div>
-            {data 
-
-         
-         
-&& <div className='row '>
-    
-    {data
-    
-    .filter((item) =>{
-        if (search === ""){
-            return item;
-        }
-        else if(item.name.toLowerCase().includes(search.toLowerCase())){
-            return item;
-        }
-    })
-    
-    .map(item=>(
-        
-       
-        
-        <div className="col-md-4 mr-3 " key={item.id}>
-         <div className="item"> 
-         <div className="image">  
-        <img src={item.image_url}/> 
-        <span className="close-btn">
-            <i className="fas fa-window-close"/>
-        </span>
         </div>
-        <div>
-        <h3 className="header1"> {item.name} </h3>
-        
-        <h4 className="header2">{item.category}</h4>
-        </div>
-        <div>
-        <h5 className="header3"> instruction 
-        <span key={item.id}  onClick={() => setToggle(!toggle)}>
-            <i className="fas fa-caret-square-down" />
-        </span>
-            </h5>
-            {toggle &&  (<p className="para1"> {item.instructions}</p>)}
-            <button className="btn btn-primary button" onClick={() => setReview(!review)} > Review</button>
-            {/* {review &&  (<Review/>)} */}
-      </div>
-     
-      </div>
-       
-        
-     
-
- </div>
-
-))}
-
-
-
-</div>
-}
         </>
     )
+        
 }
 export default Home;
